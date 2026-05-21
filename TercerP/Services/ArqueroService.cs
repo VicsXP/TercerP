722867f7d8fc04a27a1cfb4e3fad1e5664a40176
victo <victo@VICS>
2026-05-21 08:48:00 -0600

    ArqueroService Complete

--- TercerP/Services/ArqueroService.cs
+++ TercerP/Services/ArqueroService.cs
@@ -1,6 +1,45 @@
-﻿namespace TercerP.Services
+﻿using System.Text.Json;
+using TercerP.Models;
+
+namespace TercerP.Services
 {
     public class ArqueroService
     {
+        private readonly string filePath =
+            Path.Combine("wwwroot", "data", "arqueros.json");
+
+        public List<Arquero> ObtenerArqueros()
+        {
+            if (!File.Exists(filePath))
+            {
+                return new List<Arquero>();
+            }
+
+            string json = File.ReadAllText(filePath);
+
+            if (string.IsNullOrWhiteSpace(json))
+            {
+                return new List<Arquero>();
+            }
+
+            return JsonSerializer.Deserialize<List<Arquero>>(json)
+                   ?? new List<Arquero>();
+        }
+
+        public void GuardarArquero(Arquero arquero)
+        {
+            List<Arquero> arqueros = ObtenerArqueros();
+
+            arqueros.Add(arquero);
+
+            string json = JsonSerializer.Serialize(
+                arqueros,
+                new JsonSerializerOptions
+                {
+                    WriteIndented = true
+                });
+
+            File.WriteAllText(filePath, json);
+        }
     }
 }
